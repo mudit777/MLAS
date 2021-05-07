4ceef9a5cf905901da229b8c098a346d4236d4e0
Dhruwaksh Dave <darthvader@Dhruwakshs-MacBook-Pro.local>
2021-05-07 15:50:26 -0700

    Some changes

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -61,7 +61,7 @@ s3 = boto3.client('s3',
                     aws_access_key_id='******************',
                     aws_secret_access_key= '***********************',
                     # aws_session_token='secret token here'
-                     )
+                    )
 BUCKET_NAME='mlaas-cmpe295b'
 
 def create_app():
